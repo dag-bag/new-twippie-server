c39dfe56b4633383bbd86e5e013259fd215b72d5
dag-bag <virenderkumar23435@gmail.com>
2023-03-06 14:58:44 +0530

    New Plugins Update

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,20 @@
 module.exports = ({ env }) => ({
+  "strapi-paypal": {
+    enabled: true,
+    config: {
+      paypalSandBoxUrl:
+        env("PAYPAL_SANDBOX_API_URL") || "https://api-m.sandbox.paypal.com",
+      paypalLiveUrl: env("PAYPAL_LIVE_API_URL") || "https://api-m.paypal.com",
+    },
+  },
+  sitemap: {
+    enabled: true,
+    config: {
+      autoGenerate: true,
+      allowedFields: ["id", "uid"],
+      excludedTypes: [],
+    },
+  },
   seo: {
     enabled: true,
   },
